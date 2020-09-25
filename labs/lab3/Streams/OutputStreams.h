ffa843d168bd6da7e737b0ac294ae78641a88510
AnyaGl <gladysheva_2000@inbox.ru>
2020-09-25 23:57:13 +0300

    Добавила реализацию класса CFileOutputStream

--- labs/lab3/Streams/OutputStreams.h
+++ labs/lab3/Streams/OutputStreams.h
@@ -22,16 +22,31 @@ class CFileOutputStream : public IOutputDataStream
 
 public:
 	CFileOutputStream(const std::string& fileName)
+		: m_stream(fileName, std::ios::binary)
 	{
+		if (!m_stream.is_open())
+		{
+			throw std::ios_base::failure(fileName + " could not be opened or created");
+		}
 	}
 
 	void WriteByte(uint8_t data) override
 	{
+		WriteBlock(&data, 1);
 	}
 
 	void WriteBlock(const void* srcData, std::streamsize size) override
 	{
+		m_stream.write(static_cast<const char*>(srcData), size);
+		if (m_stream.fail() || m_stream.bad())
+		{
+			throw std::ios_base::failure("Failed to write block of bytes to file");
+		}
+		m_stream.flush();
 	}
+
+private:
+	std::ofstream m_stream;
 };
 
 class CMemoryOutputStream : public IOutputDataStream
