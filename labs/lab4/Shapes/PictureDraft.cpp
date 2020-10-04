e15cf9daa022c3605ecfd5bce20f5cdff38dadc2
AnyaGl <gladysheva_2000@inbox.ru>
2020-10-04 17:48:02 +0300

    Добавила реализацию класса PictureDraft

--- labs/lab4/Shapes/PictureDraft.cpp
+++ labs/lab4/Shapes/PictureDraft.cpp
@@ -1,4 +1,5 @@
 #include "PictureDraft.h"
+#include <stdexcept>
 
 size_t CPictureDraft::GetShapeCount() const
 {
@@ -7,9 +8,15 @@ size_t CPictureDraft::GetShapeCount() const
 
 CShape& CPictureDraft::GetShape(size_t index) const
 {
-	throw std::exception("need to write an implementation");
+	if (index >= m_shapes.size())
+	{
+		throw std::out_of_range("Index is out of range");	
+	}
+
+	return *m_shapes.at(index).get();
 }
 
 void CPictureDraft::AddShape(std::unique_ptr<CShape>&& shape)
 {
+	m_shapes.push_back(std::move(shape));
 }
