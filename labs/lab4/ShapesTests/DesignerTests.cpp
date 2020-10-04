95202581a3b2ceec7ef97ea581fac7d5a7681ba6
AnyaGl <gladysheva_2000@inbox.ru>
2020-10-04 13:33:10 +0300

    Добавила тесты для класса Designer

---
+++ labs/lab4/ShapesTests/DesignerTests.cpp
@@ -0,0 +1,32 @@
+#include "Designer.h"
+#include "ShapeFactory.h"
+#include "Triangle.h"
+#include "catch2/catch.hpp"
+#include <sstream>
+
+TEST_CASE("Designer must create picture draft from input stream")
+{
+	CShapeFactory factory;
+	CDesigner designer(factory);
+
+	std::stringstream ss;
+	ss << "Rectangle red 5.1 4.2 2 1\nTriangle blue 0 0 10 10 10 0";
+
+	CPictureDraft draft = designer.CreateDraft(ss);
+
+	CHECK(draft.GetShapeCount() == 2);
+
+	CTriangle draftTriangle = dynamic_cast<CTriangle&>(draft.GetShape(1));
+	CHECK(draftTriangle.GetColor() == Color::Blue);
+	CHECK(draftTriangle.GetVertex1() == Point{ 0, 0 });
+	CHECK(draftTriangle.GetVertex2() == Point{ 10, 10 });
+	CHECK(draftTriangle.GetVertex3() == Point{ 10, 0 });
+
+	SECTION("If stream is invalid, an exception must be thrown")
+	{
+		std::stringstream invalidStream;
+		invalidStream << "Rectangle red a5.1 4.2 2 1";
+
+		CHECK_THROWS(designer.CreateDraft(invalidStream));
+	}
+}
