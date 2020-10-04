c0f41341376637beba2e2b507a977cff150b44fe
AnyaGl <gladysheva_2000@inbox.ru>
2020-10-04 13:20:57 +0300

    Доработала класс Designer

--- labs/lab4/Shapes/Designer.cpp
+++ labs/lab4/Shapes/Designer.cpp
@@ -1,5 +1,10 @@
 #include "Designer.h"
 
+CDesigner::CDesigner(IShapeFactory& factory)
+	: m_factory(factory)
+{
+}
+
 CPictureDraft CDesigner::CreateDraft(std::istream& stream) const
 {
 	return CPictureDraft{};
--- labs/lab4/Shapes/Designer.h
+++ labs/lab4/Shapes/Designer.h
@@ -1,9 +1,14 @@
 #pragma once
 #include "IDesigner.h"
+#include "IShapeFactory.h"
 
 class CDesigner : public IDesigner
 {
 
 public:
+	CDesigner(IShapeFactory& factory);
 	CPictureDraft CreateDraft(std::istream& stream) const override;
+
+private:
+	IShapeFactory& m_factory;
 };
