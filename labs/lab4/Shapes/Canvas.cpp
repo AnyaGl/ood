e0bc97bf30629e0315ba97c02e4afc1c45bb29f2
AnyaGl <gladysheva_2000@inbox.ru>
2020-10-04 20:08:35 +0300

    Добавила рисование на холсте с помощью SFML

--- labs/lab4/Shapes/Canvas.cpp
+++ labs/lab4/Shapes/Canvas.cpp
@@ -1,14 +1,67 @@
 #include "Canvas.h"
 
+CCanvas::CCanvas(sf::RenderTarget& window)
+	: m_window(window)
+{
+	m_width = window.getSize().x;
+	m_height = window.getSize().y;
+}
+
 void CCanvas::SetColor(Color color)
 {
-	m_color = color;
+	if (color == Color::Black)
+	{
+		m_color = sf::Color::Black;
+	}
+	else if (color == Color::Blue)
+	{
+		m_color = sf::Color::Blue;
+	}
+	else if (color == Color::Green)
+	{
+		m_color = sf::Color::Green;
+	}
+	else if (color == Color::Pink)
+	{
+		m_color = sf::Color(255, 20, 147);
+	}
+	else if (color == Color::Red)
+	{
+		m_color = sf::Color::Red;
+	}
+	else if (color == Color::Yellow)
+	{
+		m_color = sf::Color::Yellow;
+	}
 }
 
 void CCanvas::DrawLine(Point const& from, Point const& to)
 {
+	sf::Vertex line[] = {
+		sf::Vertex(GetCoordOnCanvas(from)),
+		sf::Vertex(GetCoordOnCanvas(to))
+	};
+	line[0].color = m_color;
+	line[1].color = m_color;
+
+	m_window.draw(line, 2, sf::Lines);
 }
 
 void CCanvas::DrawEllipse(Point const& center, double horizontalRadius, double verticalRadius)
 {
+	sf::CircleShape circle((float)horizontalRadius);
+	circle.setOrigin((float)horizontalRadius, (float)horizontalRadius);
+	circle.move(GetCoordOnCanvas(center));
+	circle.setScale(1.f, float(verticalRadius / horizontalRadius));
+
+	circle.setOutlineColor(m_color);
+	circle.setOutlineThickness(3.f);
+	circle.setFillColor(sf::Color(0, 0, 0, 0));
+
+	m_window.draw(circle);
+}
+
+sf::Vector2f CCanvas::GetCoordOnCanvas(Point const& point)
+{
+	return sf::Vector2f((float)point.x, m_height - (float)point.y);
 }
--- labs/lab4/Shapes/Canvas.h
+++ labs/lab4/Shapes/Canvas.h
@@ -1,15 +1,22 @@
 #pragma once
 #include "ICanvas.h"
 #include "Color.h"
+#include "SFML/Graphics.hpp"
 
 class CCanvas : public ICanvas
 {
 
 public:
+	CCanvas(sf::RenderTarget& window);
 	void SetColor(Color color) override;
 	void DrawLine(Point const& from, Point const& to) override;
 	void DrawEllipse(Point const& center, double horizontalRadius, double verticalRadius) override;
 
 private:
-	Color m_color;
+	sf::Vector2f GetCoordOnCanvas(Point const& point);
+
+	sf::RenderTarget& m_window;
+	int m_width;
+	int m_height;
+	sf::Color m_color;
 };
